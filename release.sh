df7bbb058e4c6d1a1c26fe3a0039028a5453e68c
dev-yakuza <dev.yakuza@gmail.com>
2020-12-16 11:37:56 +0900

    change release

--- release.sh
+++ release.sh
@@ -2,6 +2,11 @@ bundle install
 bundle exec jekyll build
 python3 remove_space_href.py
 cp CNAME _site/
+
+git add .
+git commit -m 'release'
+git push origin main
+
 cd _site/
 git add .
 git commit -m 'release'
