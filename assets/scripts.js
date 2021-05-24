d43a8a80a7add62aff1e600a67869ac8b1f00e76
dev-yakuza <dev.yakuza@gmail.com>
2021-05-24 15:16:21 +0900

    fix image link bug

--- assets/scripts.js
+++ assets/scripts.js
@@ -2,7 +2,8 @@ $(document).ready(function () {
   $('article img').each(function (index, item) {
     if (
       $(item).attr('data-src') &&
-      $(item).attr('data-src').indexOf('download') === -1
+      $(item).attr('data-src').indexOf('download') === -1 &&
+      $(item).attr('src').indexOf('coupang') === -1
     ) {
       $(item).css('cursor', 'pointer');
       $(item).click(function () {
@@ -11,7 +12,8 @@ $(document).ready(function () {
       });
     } else if (
       $(item).attr('src') &&
-      $(item).attr('src').indexOf('download') === -1
+      $(item).attr('src').indexOf('download') === -1 &&
+      $(item).attr('src').indexOf('coupang') === -1
     ) {
       $(item).css('cursor', 'pointer');
       $(item).click(function () {
